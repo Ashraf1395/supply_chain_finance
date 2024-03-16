538dbee9d9f91a3c4429cef64f74d3ff84329750
Mohd Ashraf <122676159+Ashraf1395@users.noreply.github.com>
2024-03-16 05:55:27 +0000

    Update from Local

--- business_transformations/models/staging/dim_customer.sql
+++ business_transformations/models/staging/dim_customer.sql
@@ -6,15 +6,16 @@
 
 select
     -- identifiers
-    {{ dbt.safe_cast("`Customer Id`", api.Column.translate_type("integer")) }} as customer_id,
-    {{ dbt.safe_cast("`Customer Email`", api.Column.translate_type("string")) }} as customer_email,
-    {{ dbt.safe_cast("`Customer Fname`", api.Column.translate_type("string")) }} as customer_fname,
-    {{ dbt.safe_cast("`Customer Lname`", api.Column.translate_type("string")) }} as customer_lname,
-    {{ dbt.safe_cast("`Customer Segment`", api.Column.translate_type("string")) }} as customer_segment,
-    {{ dbt.safe_cast("`Customer City`", api.Column.translate_type("string")) }} as customer_city,
-    {{ dbt.safe_cast("`Customer Country`", api.Column.translate_type("string")) }} as customer_country,
-    {{ dbt.safe_cast("`Customer State`", api.Column.translate_type("string")) }} as customer_state,
-    {{ dbt.safe_cast("`Customer Street`", api.Column.translate_type("string")) }} as customer_street,
-    {{ dbt.safe_cast("`Customer Zipcode`", api.Column.translate_type("string")) }} as customer_zipcode
+    {{ dbt.safe_cast(`Customer_Id`, api.Column.translate_type("integer")) }} as customer_id,
+    {{ dbt.safe_cast("Customer_Email", api.Column.translate_type("string")) }} as customer_email,
+    {{ dbt.safe_cast("Customer_Fname", api.Column.translate_type("string")) }} as customer_fname,
+    {{ dbt.safe_cast("Customer_Lname", api.Column.translate_type("string")) }} as customer_lname,
+    {{ dbt.safe_cast("Customer_Segment", api.Column.translate_type("string")) }} as customer_segment,
+    {{ dbt.safe_cast("Customer_City", api.Column.translate_type("string")) }} as customer_city,
+    {{ dbt.safe_cast("Customer_Country", api.Column.translate_type("string")) }} as customer_country,
+    {{ dbt.safe_cast("Customer_State", api.Column.translate_type("string")) }} as customer_state,
+    {{ dbt.safe_cast("Customer_Street", api.Column.translate_type("string")) }} as customer_street,
+    {{ dbt.safe_cast("Customer_Zipcode", api.Column.translate_type("string")) }} as customer_zipcode
+    
 
 from {{ source('staging','dim_customer') }}
