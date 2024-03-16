6b8d85d199ad5e1d6d35699c1e09dbf785d9562b
Mohd Ashraf <122676159+Ashraf1395@users.noreply.github.com>
2024-03-16 06:14:58 +0000

    Update from Local

--- business_transformations/models/staging/dim_metadata.sql
+++ business_transformations/models/staging/dim_metadata.sql
@@ -6,10 +6,10 @@
 
 select
     -- attributes
-    {{ dbt.safe_cast("key", api.Column.translate_type("float")) }} as metadata_key,
-    {{ dbt.safe_cast("offset", api.Column.translate_type("integer")) }} as metadata_offset,
-    {{ dbt.safe_cast("PARTITION", api.Column.translate_type("integer")) }} as metadata_partition,
-    {{ dbt.safe_cast("time", api.Column.translate_type("integer")) }} as metadata_time,
-    {{ dbt.safe_cast("topic", api.Column.translate_type("string")) }} as metadata_topic
+    {{ dbt.safe_cast("`key`", api.Column.translate_type("float")) }} as metadata_key,
+    {{ dbt.safe_cast("`offset`", api.Column.translate_type("integer")) }} as metadata_offset,
+    {{ dbt.safe_cast("`partition`", api.Column.translate_type("integer")) }} as metadata_partition,
+    {{ dbt.safe_cast("`time`", api.Column.translate_type("integer")) }} as metadata_time,
+    {{ dbt.safe_cast("`topic`", api.Column.translate_type("string")) }} as metadata_topic
 
 from {{ source('staging','dim_metadata') }}
