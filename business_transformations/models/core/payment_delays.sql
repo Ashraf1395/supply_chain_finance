0809b84fe88387415949fb26e10d92167a3f92a0
Mohd Ashraf <122676159+Ashraf1395@users.noreply.github.com>
2024-03-17 05:06:53 +0000

    Update from Local

--- business_transformations/models/core/payment_delays.sql
+++ business_transformations/models/core/payment_delays.sql
@@ -7,5 +7,5 @@ select
     delivery_status,
     AVG(days_for_shipping_real - days_for_shipping_scheduled) AS avg_payment_delay,
     COUNT(1) AS count_of_orders
-FROM {{ ref('dim_shopping' )}}
+FROM {{ ref('dim_shipping') }}
 GROUP BY delivery_status
